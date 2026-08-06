cb7786f3109add2396810628c3a2a76f435493cd
hugoniz <hugoniz78@gmail.com>
2026-08-06 15:47:37 -0300

    Remove interactive elements and documentation from Flask project; delete app.py, HTML, CSS, JS, and error explanation files. Add SQL script for creating 'clientes' table in 'clinica_estetica' database.

--- Projetos/Projetos Aleátorios/busca_yt/coordenadas.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(5)  # Tempo para posicionar o mouse manualmente
-print(pyautogui.position())  # Mostra (X, Y) atual do cursor
-
-# Barra de pesquisa You Tube - Point(x=561, y=129)
