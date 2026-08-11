35743d88988dc4deea1716e30462f643ea7ff413
Hugo Niz <114558836+nizcomz@users.noreply.github.com>
2026-08-11 13:10:36 +0000

    feat: adiciona funcionalidade de depósito em sistema bancário

---
+++ JavaScript/sistemBanco.js
@@ -0,0 +1,11 @@
+let conta = {
+    titular: "Ana",
+    saldo: 150
+};
+
+function depositar(conta,valor) {
+    conta.saldo = conta.saldo + valor;
+    console.log('Depósito de ${valor} realizado. Novo saldo: ${conta.saldo}')
+}
+
+depositar(conta, 50)
