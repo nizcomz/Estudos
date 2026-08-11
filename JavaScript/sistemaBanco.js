6879124de8eaa622ef9cdc8543b3d159f2e5b468
Hugo Niz <114558836+nizcomz@users.noreply.github.com>
2026-08-11 13:47:34 +0000

    refactor: ajusta formatação e espaçamento nas funções de depósito e saque

--- JavaScript/sistemaBanco.js
+++ JavaScript/sistemaBanco.js
@@ -3,9 +3,23 @@ let conta = {
     saldo: 150
 };
 
-function depositar(conta,valor) {
+function depositar(conta, valor) {
     conta.saldo = conta.saldo + valor;
     console.log(`Depósito de ${valor} realizado. Novo saldo: ${conta.saldo}`);
 }
 
+
+function saque(conta, valor) {
+    if (conta.saldo >= valor) {
+        conta.saldo -= valor;
+        console.log(`Saque de ${valor} realizado com sucesso! Novo saldo: ${conta.saldo}`);
+    } else {
+        console.log(`Saldo insuficiente!`);
+    }
+
+}
+
+
+saque(conta, 100);
 depositar(conta, 50);
+
